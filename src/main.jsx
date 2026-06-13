f81ae780684187e4e488f9c0a9ca0511d2b560f3
Wisdom Changala <wisdomchangala@gmail.com>
2026-06-14 00:05:19 +0200

    updated font

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/nunito/900.css';
 import App from './App.jsx';
 import './styles.css';
 
